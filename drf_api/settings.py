fc135078646ba4bc2777bd5297420fe6a2a0481c
Alsona Natsi <alsona.naci1188@gmail.com>
2024-08-27 03:36:33 +0000

    Removing the trusted

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -126,11 +126,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'drf_api.urls'
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://drf-api-alsona-0c809e0777a5.herokuapp.com',
-    'https://*.gitpod.io',
-]
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
