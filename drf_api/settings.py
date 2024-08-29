c90dc37b66966f0d5b87799b7809b10d46ed7d4d
alsona1188 <146828222+alsona1188@users.noreply.github.com>
2024-08-29 05:23:07 +0200

    Update settings.py
    update

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -86,6 +86,11 @@ if "CLIENT_ORIGIN_DEV" in os.environ:
 
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://moments-alsona-cf3b091c83b4.herokuapp.com",
+]
+
+
 # Application definition
 
 INSTALLED_APPS = [
